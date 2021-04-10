55d5259bc4956b37acb9c586db19dc60688f5cb6
86136 <>
2021-04-11 00:26:55 +0800

    陈昌隆4.11_改了点射击bug

--- Assets/Scripts/Skills/Sk1_FastShoot.cs
+++ Assets/Scripts/Skills/Sk1_FastShoot.cs
@@ -20,15 +20,9 @@ public class Sk1_FastShoot : BaseShoot
     //覆盖，返回射击方向
     protected override Vector3 Shoot_GetShootDir()
     {
-        Vector3 mousePos = Input.mousePosition;//鼠标位置
-        //转世界坐标,有个坑是z坐标不能为0，很烦,好像使用摄像机的z绝对值才比较准确
-        float newZ = Mathf.Abs(cameraTransform.position.z);
-        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, newZ));
+        Vector3 mousePos = Mouse3D.GetMousePosition();
 
         Vector3 v = mousePos - new Vector3(firePoint.position.x, firePoint.position.y, 0);
-        Debug.Log("鼠标坐标" + mousePos.ToString());
-        Debug.Log("开火点坐标" + firePoint.transform.position.ToString());
-        Debug.Log("两者差值" + v.ToString());
 
         firePoint.transform.rotation = Quaternion.Euler(v);
 
