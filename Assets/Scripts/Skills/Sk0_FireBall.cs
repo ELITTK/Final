84f764fb2199773bef92a438f63237b3186a05c3
1411849810@qq.com <>
2021-04-15 13:37:07 +0800

    Initial commit

---
+++ Assets/Scripts/Skills/Sk0_FireBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sk0_FireBall : BaseShoot
+{
+    private Transform cameraTransform;
+
+    public void Awake()
+    {
+        cameraTransform = GameObject.Find("Main Camera").transform;
+    }
+
+    public void Start()
+    {
+        Shoot();
+    }
+
+
+    //覆盖，返回射击方向
+    protected override Vector3 Shoot_GetShootDir()
+    {
+        Vector3 mousePos = Mouse3D.GetMousePosition();
+
+        Vector3 v = mousePos - new Vector3 (firePoint.position.x, firePoint.position.y, 0);
+
+        return v;
+    }
+}
---
+++ Assets/Scripts/Skills/SkillData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skill/New Skill")]
+public class SkillData : ScriptableObject
+{
+    public int skillID;
+    public Sprite skillSpirte;//技能图片
+
+    public string skillName;
+    [TextArea]
+    public string skillDes;//技能描述
+
+    public GameObject skillCaster;//技能释放器
+
+    public float maxCd = 1;
+
+
+    public void UseSkill(Transform ownerTrans)
+    {
+        //给技能释放者生成一个指定的技能释放器，让它来触发具体技能效果。
+        GameObject tempCaster = Instantiate(skillCaster, ownerTrans);
+        Destroy(tempCaster, 1);
+    }
+}
