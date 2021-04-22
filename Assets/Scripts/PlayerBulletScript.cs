7a455338edd87eeeaf33170539a1e5a8e29d702c
86136 <>
2021-04-22 20:36:42 +0800

    ccl_玩家子弹碰撞敌人时正确关闭粒子系统

--- Assets/Scripts/PlayerBulletScript.cs
+++ Assets/Scripts/PlayerBulletScript.cs
@@ -6,11 +6,18 @@ public class PlayerBulletScript : MonoBehaviour
 {
     public float bulletDmg;
 
+    private float lifeTime = 1f;//一旦子弹存活超过这个时间，强制让子弹失效
+    private bool isHidden = false;//子弹是否已经执行了HideBullet，目前只参与子弹计时失效
+
     private void Start()
     {
-
         var main = GetComponent<ParticleSystem>().main;
         main.stopAction = ParticleSystemStopAction.Callback;
+    }
+
+    private void OnEnable()
+    {
+        Invoke("StopAllParticleSystem", lifeTime);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,20 +31,38 @@ public class PlayerBulletScript : MonoBehaviour
 
     private void StopAllParticleSystem()
     {
-        //不让粒子系统再生成新粒子
-        ParticleSystem[] particleSystems;
-         particleSystems = GetComponentsInChildren<ParticleSystem>();
-
-        foreach (ParticleSystem ps in particleSystems)
+        if (!isHidden)
         {
-            ps.Stop();
+            //隐藏子弹球体
+            HideBullet();
+            //停止粒子系统，不让粒子系统再生成新粒子
+            ParticleSystem[] particleSystems;
+            particleSystems = GetComponentsInChildren<ParticleSystem>();
+
+            foreach (ParticleSystem ps in particleSystems)
+            {
+                ps.Stop();
+            }
         }
     }
 
     private void OnParticleSystemStopped()
     {
         //当该物体已经没有存活的粒子时，隐藏物体以便让对象池再次调用
+
+        UnHideBullet();//解除子弹球体的隐藏
         gameObject.SetActive(false);
-        Debug.Log("所有粒子已死亡");
+    }
+
+    private void HideBullet()
+    {
+        isHidden = true;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+    }
+    private void UnHideBullet()
+    {
+        isHidden = false;
+        GetComponent<MeshRenderer>().enabled = true;
     }
 }
