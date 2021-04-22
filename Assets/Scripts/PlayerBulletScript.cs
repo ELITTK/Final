0a2511e2bbd1ae9dc956396b59f3896254dd30b7
86136 <>
2021-04-22 19:52:36 +0800

    ccl_改bug 使用时请以SampleScene为准

--- Assets/Scripts/PlayerBulletScript.cs
+++ Assets/Scripts/PlayerBulletScript.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class PlayerBulletScript : MonoBehaviour
 {
     public float bulletDmg;
-    
+
+    private void Start()
+    {
+
+        var main = GetComponent<ParticleSystem>().main;
+        main.stopAction = ParticleSystemStopAction.Callback;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+        //碰撞事件
         if (!collision.gameObject.CompareTag("Player"))
         {
-            gameObject.SetActive(false);
-
-
+            StopAllParticleSystem();
         }
+    }
+
+    private void StopAllParticleSystem()
+    {
+        //不让粒子系统再生成新粒子
+        ParticleSystem[] particleSystems;
+         particleSystems = GetComponentsInChildren<ParticleSystem>();
+
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.Stop();
+        }
+    }
+
+    private void OnParticleSystemStopped()
+    {
+        //当该物体已经没有存活的粒子时，隐藏物体以便让对象池再次调用
+        gameObject.SetActive(false);
+        Debug.Log("所有粒子已死亡");
     }
 }
